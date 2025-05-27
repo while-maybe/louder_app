ade1e4e2b9d8a64285b1fce3ba3c977c4c494798
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-27 05:27:22 +0100

    feat: add driving ports

---
+++ internal/core/service/message_service_impl.go
@@ -0,0 +1,30 @@
+// Implements the driving ports. Contains the core application logic. It depends on driven port interfaces, not concrete implementations. This is key for DI.
+package service
+
+import (
+	"log"
+	"louder/internal/core/domain"
+	drivenports "louder/internal/core/ports/driven" // depends on driven port - dependencies
+	drivingports "louder/internal/core/ports/driving"
+)
+
+type messageServiceImpl struct {
+	dataRepo drivenports.DataRepository // Injected dependency
+}
+
+// NewMessageService is the constructor for messageServiceImpl
+func NewMessageService(db drivenports.DataRepository) drivingports.MessageService { // returns the driving port interface
+	return &messageServiceImpl{
+		dataRepo: db,
+	}
+}
+
+func (m *messageServiceImpl) GetMessage() domain.MsgWithTime {
+	log.Println("Getting a message from db...")
+	return m.dataRepo.GetDBMessage()
+}
+
+func (m *messageServiceImpl) GetRandomNumber() domain.RandomNumber {
+	log.Println("Getting a random number from db...")
+	return m.dataRepo.GetDBRandomNumber()
+}
