d9c9f3577020eae669b36149d39ac7a28a700ac9
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-28 04:07:27 +0100

    refactor: add all DI logic

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"log"
+	dbdriven "louder/internal/adapters/driven/mock_db"
+	apidriving "louder/internal/adapters/driving/api_provider/stdlib"
+	coreservice "louder/internal/core/service"
+	"os"
 )
 
 func main() {
-	log.Println("Starting louder...")
+	log.Print("LOUDER: ")
+
+	serverAddr := os.Getenv("SERVER_ADDR")
+
+	// instantiate driven adapters
+	dataRepo := dbdriven.NewMockDBMessageRepository("Message With Time")
+	randomRepo := dbdriven.NewMockDBRandRepository("Random number")
+
+	// instantiate core app services
+	messageService := coreservice.NewMessageService(dataRepo)
+	randomNumberService := coreservice.NewRandNumberService(randomRepo)
+
+	// instantiate driving adapters
+	messageHandler := apidriving.NewMessageHandler(messageService)
+	randomNumberHandler := apidriving.NewRandomNumberHandler(randomNumberService)
+
+	// instantiate router
+	router := apidriving.NewRouter(messageHandler, randomNumberHandler)
+
+	// start the server
+	log.Printf("Starting server on port %s\n", serverAddr)
+	apidriving.StartServer(router, serverAddr)
+	log.Println("Shutting server down")
 }
