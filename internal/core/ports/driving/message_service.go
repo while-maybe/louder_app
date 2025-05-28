0e48d6788a03b9dfc6dc34034fa721a90ace529e
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-28 04:11:25 +0100

    refactor(service): tidy up

--- internal/core/ports/driving/message_service.go
+++ internal/core/ports/driving/message_service.go
@@ -5,5 +5,8 @@ import "louder/internal/core/domain"
 // MessageService defines the primary use case for Messages - What do we do with Messages?
 type MessageService interface {
 	GetMessage() domain.MsgWithTime
+}
+
+type RandomNumberService interface {
 	GetRandomNumber() domain.RandomNumber
 }
