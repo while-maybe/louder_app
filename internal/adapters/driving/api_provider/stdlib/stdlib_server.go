6c3300c1b7c81d8e3b541581b4ffe02ef7870e45
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-06-03 00:18:57 +0100

    refactor: implement graceful stdlib http server shutdown
    Removed the old StartServer function and the invalid httpServer declaration.
    Created an APIServer struct to hold the *http.Server.
    NewAPIServer is a constructor that properly initializes the server with timeouts.
    ListenAndServe() is now a method on our struct.
    Added a Shutdown() method which is the key to enabling graceful shutdown from main.

--- internal/adapters/driving/api_provider/stdlib/stdlib_server.go
+++ internal/adapters/driving/api_provider/stdlib/stdlib_server.go
@@ -1,14 +1,46 @@
 package stdlibapiadapter
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
-	"os"
+	"time"
 )
 
-// const (
-// 	DefaultServerAddress = ":8080"
-// )
+type StdAPIServer struct {
+	httpServer *http.Server
+}
+
+// NewStdAPIServer creates and configures a new server and we configure timeouts here
+func NewStdAPIServer(addr string, router http.Handler) *StdAPIServer {
+	httpServer := &http.Server{
+		Addr:    addr,
+		Handler: router,
+		// timeouts to prevent slow-client attacks
+		ReadTimeout:  5 * time.Second,   // Max time entire request including body
+		WriteTimeout: 10 * time.Second,  // Max time before writes time out
+		IdleTimeout:  120 * time.Second, // Max time to wait for new request on a keep-alive connection
+	}
+	return &StdAPIServer{
+		httpServer: httpServer,
+	}
+}
+
+// ListenAndServe starts the HTTP server. It's a blocking call.
+func (s *StdAPIServer) ListenAndServe() error {
+	log.Printf("Starting net/http server on %s", s.httpServer.Addr)
+	if err := s.httpServer.ListenAndServe(); err != nil {
+		return err // return any error that is not graceful shutdown http.ErrServerClosed
+	}
+
+	return nil
+}
+
+// Shutdown gracefully shuts down the HTTP server
+func (s *StdAPIServer) Shutdown(ctx context.Context) error {
+	log.Printf("Shutting down net/http gracefully...")
+	return s.httpServer.Shutdown(ctx)
+}
 
 func StartServer(router http.Handler, addr string) {
 	// if addr == "" {
@@ -16,9 +48,9 @@ func StartServer(router http.Handler, addr string) {
 	// 	addr = DefaultServerAddress
 	// }
 
-	err := http.ListenAndServe(addr, router)
+	err := http.ListenAndServe(":"+addr, router)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		log.Fatalf("FATAL: API server failed: %v", err)
+
 	}
 }
