ab49e647ad3985acb4a7d90451b9c608952de97e
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-06-03 01:11:15 +0100

    feat: add GetPersonRoute

--- internal/adapters/driving/api_provider/stdlib/router.go
+++ internal/adapters/driving/api_provider/stdlib/router.go
@@ -5,12 +5,23 @@ import "net/http"
 const (
 	NewMessageRoute      = "/message"
 	NewRandomNumberRoute = "/random"
+	NewPersonRoute       = "/person"
+	GetPersonRoute       = "/person/"
 )
 
-func NewRouter(messageHandler *MessageHandler, randomNumberHandler *RandomNumberHandler) *http.ServeMux {
+func NewRouter(
+	messageHandler *MessageHandler,
+	randomNumberHandler *RandomNumberHandler,
+	personHandler *PersonHandler,
+) *http.ServeMux {
 	r := http.NewServeMux()
 	// Go 1.22+ handles the METHOD /path pattern
+	// Message, RandomNumber
 	r.HandleFunc(http.MethodGet+" "+NewMessageRoute, messageHandler.HandleGetMessage)
 	r.HandleFunc(http.MethodGet+" "+NewRandomNumberRoute, randomNumberHandler.HandleGetRandomNumber)
+	// Person
+	r.HandleFunc(http.MethodPost+" "+NewPersonRoute, personHandler.HandleCreatePerson)
+	// r.HandleFunc(http.MethodGet+" "+GetPersonRoute, personHandler.HandleGetPerson)
+	// TODO research timout on individual routes
 	return r
 }
