50a72fa8f53de5a1bdce936ad4333af5e85e24c2
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-06-20 05:16:13 +0100

    refactor: update currency repository tests

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/gofrs/uuid/v5 v5.3.2
 	github.com/golang-migrate/migrate/v4 v4.18.3
+	github.com/google/go-cmp v0.7.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.28
 	github.com/uptrace/bun v1.2.11
--- internal/adapters/driven/db/sqlx_adapter/currency_repo_test.go
+++ internal/adapters/driven/db/sqlx_adapter/currency_repo_test.go
@@ -1,10 +1,13 @@
 package sqlxadapter_test
 
 import (
+	"context"
 	"database/sql"
 	"errors"
 	"fmt"
 	"log"
+	sqlxadapter "louder/internal/adapters/driven/db/sqlx_adapter"
+	"louder/internal/core/domain"
 	"os"
 	"path"
 	"path/filepath"
@@ -20,7 +23,7 @@ import (
 	_ "github.com/mattn/go-sqlite3" // SQLite driver
 )
 
-// runs once at module load and changes working directory to project root, adjust the ../../.. as needed
+// runs once at module load and changes working directory to project root, adjust the ../../../../.. as needed
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
 	dir := path.Join(filename, "../../../../../..")
@@ -37,10 +40,9 @@ func setupTestDB(t *testing.T) (*sqlx.DB, func()) {
 
 	// dsn := "file::memory:?cache=shared"
 	// tests manage the temp dir so it self cleans after testing
-	// sqlite pragma options: fkeys on and wal on
 
 	tempDBFilePath := filepath.Join(t.TempDir(), fmt.Sprintf("test_db_%d.sqlite", time.Now().UnixNano()))
-
+	// sqlite pragma options: fkeys on and wal on
 	dbOptions := "?cache=shared&_pragma=foreign_keys(1)&_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)"
 
 	dsn := fmt.Sprintf("file:%s%s", tempDBFilePath, dbOptions)
@@ -56,7 +58,7 @@ func setupTestDB(t *testing.T) (*sqlx.DB, func()) {
 		t.Fatalf("failed to ping test db")
 	}
 
-	migrationsPath := "./migrations" // relative to test file?
+	migrationsPath := "./migrations" // bless init() on top
 
 	// does migration files folder URL exist?
 	if _, err := os.Stat(migrationsPath); os.IsNotExist(err) {
@@ -68,6 +70,7 @@ func setupTestDB(t *testing.T) (*sqlx.DB, func()) {
 	migrateDSN := fmt.Sprintf("sqlite3://%s", tempDBFilePath)
 
 	// create new migration instance
+	// the sqlite3 driver is NOT mattn's - migrate has its own :/
 	m, err := migrate.New(migrationsURL, migrateDSN)
 	if err != nil {
 		tempDB.Close()
@@ -84,32 +87,85 @@ func setupTestDB(t *testing.T) (*sqlx.DB, func()) {
 		t.Fatalf("failed to apply migrations: %v", err)
 	}
 
-	log.Printf("Migrations applied to test db")
+	log.Printf("Migrations up on test db")
 
 	// call sqlx
 	sqlxDB := sqlx.NewDb(tempDB, "sqlite3")
 
-	cleanup := func() {
-		log.Println("Cleaning temp db")
+	cleanupTestDB := func() {
+		log.Println("Cleaning up test db")
 
 		// reverse the migrations
 		if err := m.Down(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
 			m.Close()
 		}
 
+		log.Printf("Migrations down on test db")
+
 		sqlxDB.Close()
 		os.Remove(tempDBFilePath)
 	}
 
-	return sqlxDB, cleanup
+	return sqlxDB, cleanupTestDB
 }
 
-func TestCurrencyRepository(t *testing.T) {
-	t.Run("Save and GetByID", func(t *testing.T) {
-		_, cleanup := setupTestDB(t)
-		defer cleanup()
+func TestSaveAndGetByID(t *testing.T) {
+	eurCode, _ := domain.NewCurrencyCode("eur")
 
-		fmt.Println("got to the test")
-	})
+	tt := map[string]struct {
+		currToSave   *domain.Currency
+		wantCurrID   domain.CurrencyCode
+		wantCurrName string
 
+		wantSaveErr error
+		wantGetErr  error
+	}{
+		"Save/Get new currency ok": {
+			currToSave: func() *domain.Currency {
+				c, _ := domain.NewCurrency(eurCode, "Euro")
+				return c
+			}(), // call it here
+			wantCurrID:   "EUR",
+			wantCurrName: "Euro",
+			wantSaveErr:  nil,
+			wantGetErr:   nil,
+		},
+		// other cases, check coverage?
+	}
+
+	// iterate through tests here
+	for name, tc := range tt {
+
+		t.Run(name, func(t *testing.T) {
+			// for db ops, best practice is to clean db and recreate per test...
+			db, cleanup := setupTestDB(t)
+			defer cleanup()
+
+			// NewCurrencyRepo needs a *sql.DB which is encapsulated inside *sqlx.db.DB, hence db.DB
+			repo, err := sqlxadapter.NewCurrencyRepo(db.DB)
+			if err != nil {
+				t.Fatalf("failed to create currency repo: %v", err)
+			}
+
+			ctx := context.Background()
+
+			// testing starts here
+			gotCurr, saveErr := repo.Save(ctx, tc.currToSave)
+
+			// save errors match
+			if tc.wantSaveErr != saveErr {
+				t.Fatalf("unexpected error: expected %v got %v", tc.wantSaveErr, saveErr)
+			}
+
+			// currency code matches
+			if tc.wantCurrID != gotCurr.Code() {
+				t.Fatalf("error unexpected currency code: expected %v got %v", tc.wantCurrID, gotCurr.Code())
+			}
+
+			// currency name matches
+			if tc.wantCurrName != gotCurr.Name() {
+				t.Fatalf("error unexpected currency name: expected %v got %v", tc.wantCurrName, gotCurr.Name())
+			}
+		})
+	}
 }
